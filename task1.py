216d8bea1762945794bf16801b2a21af9602b588
Anna Borisova <anna200310f@gmail.com>
2023-02-11 01:10:08 +0700

    Исправила задания к семинару 1.

--- task1.py
+++ task1.py
@@ -3,14 +3,13 @@ a = int(input('Введите номер месяца\n'))
 
 def season(number):
     if number in [1, 2, 12]:
-        print('зима')
-    elif number in [3,4,5]:
-        print('весна')
+        return 'зима'
+    elif number in [3, 4, 5]:
+        return 'весна'
     elif number in [6, 7, 8]:
-        print('лето')
-    else: print('осень')
+        return 'лето'
+    else:
+        return 'осень'
 
 
-season(a)
-
-
+print(season(a))
--- task4.py
+++ task4.py
@@ -1,11 +1,5 @@
-c = int(input("Введите число\n"))
-temp = c
-backward = 0
-while c > 0:
-    a = c % 10
-    backward = backward*10 + a
-    c = c // 10
-if temp == backward:
-    print('Это палиндром')
+s = input()
+if s == s[::-1]:
+    print("yes")
 else:
-    print('Это не палиндром')
+    print("no")
