310f6a510f60bbdd4680a4c659f3a98b489f675c
Anna Borisova <anna200310f@gmail.com>
2023-03-27 23:14:35 +0700

    Исправила задание к семинару 5.

--- hw5/task3.py
+++ hw5/task3.py
@@ -7,5 +7,5 @@ with open('RomeoAndJuliet.json', encoding="utf-8") as f:
     for act in cur["acts"]:
         for scene in act["scenes"]:
             for action in scene["action"]:
-                phrases += Counter([action["character"]] * len(action["says"]))
+                phrases[action["character"]] += len(action["says"])
     print(phrases.most_common())
