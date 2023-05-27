b83185e2a1d69107f13e8ff0f6ebb3d9dc6e77e4
Anna Borisova <anna200310f@gmail.com>
2023-05-27 16:38:57 +0700

    Исправила задание к семинару 13

--- hw13/task1.py
+++ hw13/task1.py
@@ -18,9 +18,8 @@ class Wordform:
 
 
 class Sentence:
-    def __init__(self, sentence):
-        for k in sentence.iter('source'):
-            self.__sentence = k.text
+    def __init__(self, sentence: etree.Element):
+        self.__sentence = sentence.find('source').text
         self.__words = []
         for k in sentence.iter('tokens'):
             for j in k.iter('token'):
@@ -38,11 +37,11 @@ class Sentence:
 class Corpus:
 
     def __init__(self, path_to_corpus):
-        self.path_to_corpus = path_to_corpus
+        self.__path_to_corpus = path_to_corpus
         self.__sentences = []
 
     def load(self):
-        tree = etree.parse(self.path_to_corpus)
+        tree = etree.parse(self.__path_to_corpus)
         root = tree.getroot()
         for i in root.iter('sentence'):
             sentence2 = Sentence(i)
@@ -50,41 +49,6 @@ class Corpus:
 
     def find_sentence(self, n):
         return self.__sentences[n]
-
-
-class Test(unittest.TestCase):
-
-    def setUp(self):
-        self.corpustest = Corpus('annot.opcorpora.no_ambig.xml')
-        self.corpustest.load()
-
-    def test_sentence(self):
-        self.assertTrue(self.corpustest.find_sentence(
-            5).printsentence() == "В остальном «Школа злословия» представляла собой интервью ведущих с героем выпуска.")
-
-    def test_word(self):
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(0) == 'ADJF')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(1) == 'Subx')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(2) == 'Apro')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(3) == 'neut')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(4) == 'sing')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).find_grammem(5) == 'loct')
-
-    def test_grammem(self):
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(0).printword() == 'В')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(1).printword() == 'остальном')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(2).printword() == '«')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(3).printword() == 'Школа')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(4).printword() == 'злословия')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(5).printword() == '»')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(6).printword() == 'представляла')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(7).printword() == 'собой')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(8).printword() == 'интервью')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(9).printword() == 'ведущих')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(10).printword() == 'с')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(11).printword() == 'героем')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(12).printword() == 'выпуска')
-        self.assertTrue(self.corpustest.find_sentence(5).find_word(13).printword() == '.')
 
 
 if __name__ == '__main__':
