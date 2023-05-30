6dff6a948056afe82ccb51a7e7e7cfea6e4d9e78
Anna Borisova <anna200310f@gmail.com>
2023-05-30 17:44:39 +0700

    Исправила задание 14

--- hw14/hw14.py
+++ hw14/hw14.py
@@ -8,41 +8,41 @@ from collections import Counter, defaultdict
 class TFIDF:
 
     def __init__(self, directory):
-        self.idf = defaultdict(float)
-        self.directory = directory
-        self.time = 0
+        self.__idf = defaultdict(float)
+        self.__directory = directory
+        self._time = 0
         self.__load_instance()
 
     def __load_instance(self):
         try:
-            with open(self.directory + ".pkl", "rb") as file:
+            with open(self.__directory + ".pkl", "rb") as file:
                 loaded_object = pickle.load(file)
-                if loaded_object.time != os.path.getmtime(self.directory):
+                if loaded_object._time != os.path.getmtime(self.__directory):
                     print("Recalculating")
                     self.__calc_instance()
                     return
-            self.time = loaded_object.time
-            self.idf = loaded_object.idf
+            self._time = loaded_object._time
+            self.__idf = loaded_object.__idf
             print("Loaded instance from file")
         except FileNotFoundError:
             self.__calc_instance()
 
     def __calc_instance(self):
         self.__calc()
-        self.time = os.path.getmtime(self.directory)
-        with open(self.directory + ".pkl", "wb") as file:
+        self._time = os.path.getmtime(self.__directory)
+        with open(self.__directory + ".pkl", "wb") as file:
             pickle.dump(self, file)
         print("Calculated")
 
     def __calc(self):
-        files = os.listdir(self.directory)
+        files = os.listdir(self.__directory)
         idf_freq = defaultdict(set)
         for file in files:
-            tf = TFIDF.calc_tf(self.directory + "/" + file)
+            tf = TFIDF.calc_tf(self.__directory + "/" + file)
             for word in tf:
                 idf_freq[word].add(file)
         for word in idf_freq:
-            self.idf[word] = math.log2(len(files) / len(idf_freq[word]))
+            self.__idf[word] = math.log2(len(files) / len(idf_freq[word]))
 
     @classmethod
     def calc_tf(cls, filename):
@@ -55,4 +55,7 @@ class TFIDF:
         return tf
 
     def calc_tf_idf(self, filename):
-        return [(word, value * self.idf[word]) for word, value in TFIDF.calc_tf(filename).items()]
+        return [(word, value * self.__idf[word]) for word, value in TFIDF.calc_tf(filename).items()]
+
+    def get_idf(self):
+        return self.__idf.copy()
--- hw14/tests.py
+++ hw14/tests.py
@@ -5,6 +5,10 @@ from hw14 import TFIDF
 
 
 class Tests(unittest.TestCase):
+
+    def setUp(self):
+        self.tfidf = TFIDF("test")
+
     def test_tf(self):
         tf = TFIDF.calc_tf("test.txt")
         self.assertAlmostEqual(tf["заплачу"], 2 / 5)
@@ -13,18 +17,18 @@ class Tests(unittest.TestCase):
         self.assertAlmostEqual(tf["кто"], 1 / 5)
 
     def test_idf(self):
-        tfidf = TFIDF("test")
-        self.assertEqual(tfidf.time, os.path.getmtime("test"))
-        self.assertEqual(len(set(tfidf.idf.values())), len(os.listdir("test")))
-        self.assertAlmostEqual(tfidf.idf["заплачу"], 2.321928094887362)
-        self.assertAlmostEqual(tfidf.idf["глупенькая"], 2.321928094887362)
-        self.assertAlmostEqual(tfidf.idf["кольцо"], 1.3219280948873624)
-        self.assertAlmostEqual(tfidf.idf["женщины"], 1.3219280948873624)
-        self.assertAlmostEqual(tfidf.idf["кто"], 0.7369655941662062)
-        self.assertAlmostEqual(tfidf.idf["с"], 0.32192809488736235)
+        self.assertEqual(self.tfidf._time, os.path.getmtime("test"))
+        idf = self.tfidf.get_idf()
+        self.assertEqual(len(set(idf.values())), len(os.listdir("test")))
+        self.assertAlmostEqual(idf["заплачу"], 2.321928094887362)
+        self.assertAlmostEqual(idf["глупенькая"], 2.321928094887362)
+        self.assertAlmostEqual(idf["кольцо"], 1.3219280948873624)
+        self.assertAlmostEqual(idf["женщины"], 1.3219280948873624)
+        self.assertAlmostEqual(idf["кто"], 0.7369655941662062)
+        self.assertAlmostEqual(idf["с"], 0.32192809488736235)
 
     def test_tf_idf(self):
-        tfidf = TFIDF("test").calc_tf_idf("test.txt")
+        tfidf = self.tfidf.calc_tf_idf("test.txt")
         results = {'заплачу': 0.9287712379549449,
                    'женщины': 0.2643856189774725,
                    'глупенькая': 0.46438561897747244,
